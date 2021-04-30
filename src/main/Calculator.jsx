36625bb9a3553b3267cfd03ac111a2eeeefa289a
LuisLimaIt <pls.luis@gmail.com>
2021-04-30 11:13:11 -0300

    logic implementation - mathematical operations

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -19,17 +19,38 @@ export default class Calculator extends Component {
     constructor(props) {
         super(props)
         
-        this.clearMemory = this.clearMemory.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
+        this.clearMemory = this.clearMemory.bind(this);
+        this.setOperation = this.setOperation.bind(this);
+        this.addDigit = this.addDigit.bind(this);
     }
 
     clearMemory() {
-        this.setState({ ...initialState })
+        this.setState({ ...initialState });
     }
 
     setOperation(operation) {
-        console.log(operation);
+        if(this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '=' ;
+            const currentOperation = this.state.operation;
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
